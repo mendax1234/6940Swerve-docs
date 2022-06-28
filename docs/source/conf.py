a3b1f20d66dabf4a20785878f9c6517fd70e957a
mendax1234 <daniel.wb.zhu@outlook.com>
2022-06-28 15:23:43 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = '6940 Swerve Documentation'
 copyright = '2022, Wenbo Zhu (mendax1234)'
 author = 'Wenbo Zhu (mendax1234)'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
