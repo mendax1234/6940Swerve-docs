35a4aac5f86c3661ffa261ab3e632c732b64e0a3
mendax1234 <daniel.wb.zhu@outlook.com>
2022-06-28 12:34:52 +0800

    Add dark theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,5 +62,5 @@ mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
 html_static_path = ["_static"]
 
 html_css_files = [
-    'css/rtd_dark.css',
+    "css/custom.css",
 ]
