261285fff575c890129f78507766f266359ebca9
mendax1234 <daniel.wb.zhu@outlook.com>
2022-06-30 08:28:39 +0800

    Add automatic pull and push

---
+++ scripts/update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# update transifex pot and local po files
+
+set -ex
+
+#LANG_TO_PULL=${1:-'fr_CA,es_MX,zh_CN,tr_TR,he_IL,pt'}
+LANG_TO_PULL=${1:-'zh_CN'}
+#LANG_MAP='es_MX: es, fr_CA: fr, he_IL: he, tr_TR: tr'
+MAINPROJECT=6940swerve-docs
+
+# Set working directory to repo root
+cd `dirname $0`/..
+
+# Create POT Files
+sphinx-build -T -b gettext $MAINPROJECT/source locale/pot
+
+# Update .tx/config
+rm .tx/config
+sphinx-intl create-txconfig
+echo "lang_map = ${LANG_MAP}" >> .tx/config
+sphinx-intl update-txconfig-resources -p locale/pot -d locale --transifex-project-name $MAINPROJECT
+
+# Push and pull from Transifex. It is important to push then pull!
+# If you pull then push, the PO files will contain out of date strings.
+if [ "$CI" = true ]
+then
+    tx push --source --no-interactive --skip
+fi
+tx pull -l $LANG_TO_PULL --mode onlyreviewed --use-git-timestamps
