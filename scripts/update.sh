7b441308f3021c564c4adef879e40b06db724e5c
mendax1234 <daniel.wb.zhu@outlook.com>
2022-07-01 12:34:46 +0800

    Fix file location error

--- scripts/update.sh
+++ scripts/update.sh
@@ -19,7 +19,7 @@ sphinx-build -T -b gettext docs/source docs/pot
 rm .tx/config
 sphinx-intl create-txconfig
 echo "lang_map = ${LANG_MAP}" >> .tx/config
-sphinx-intl update-txconfig-resources -p docs/pot -d locale --transifex-project-name $MAINPROJECT
+sphinx-intl update-txconfig-resources -p docs/pot -d docs/locales --transifex-project-name $MAINPROJECT
 
 # Push and pull from Transifex. It is important to push then pull!
 # If you pull then push, the PO files will contain out of date strings.
